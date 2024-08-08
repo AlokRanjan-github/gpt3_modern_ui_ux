f644353d751b14c7d139657d2605af3b0965c559
AlokRanjan-github <alokrp7007@gmail.com>
2024-08-08 13:44:07 +0530

    cta Section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -4,14 +4,14 @@ import possibilityImage from '../../assets/possibility.png';
 const Possibility = () => {
   return (
     <div className='gpt3__possibility section__padding' id='possibility'>
-      <div  className='gpt3__possibility-img'>
+      <div className='gpt3__possibility-image'>
         <img src={possibilityImage} alt="possibilityImage" />
-        <div className='gpt3__possibility-content'>
-          <h4>Request Early Access to Get Started</h4>
-          <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
-          <p>Yet bed any for travelling assistance indulgence unpleasing Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-          <h4>Request Early Access to Get Started</h4>
-        </div>
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. <br /><br /> In a world where imagination knows no bounds, the potential of language unfolds in ways once thought impossible. The written word, long the cornerstone of human expression, now finds new life through the power of artificial intelligence. Every sentence crafted with precision, every idea articulated with clarity, reflects the confluence of human creativity and technological advancement. This is not just the future of communication; it is the evolution of thought itself, a step closer to the harmonious blending of man and machine.</p>
+        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
   )
